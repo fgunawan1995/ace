5fe3ed4914b13ed6eb18953f059de5186e8d5aaf
Felix Gunawan <felixgunawan1995@gmail.com>
2018-05-24 15:35:38 +0700

    add auth

--- routes/api.php
+++ routes/api.php
@@ -28,6 +28,6 @@ Route::group(['middleware' => 'jwt.auth'], function(){
 
 Route::middleware('jwt.refresh')->get('/token/refresh', 'AuthController@refresh');
 
-Route::group(['prefix' => '/v1', 'namespace' => 'Api\V1', 'as' => 'api.'], function () {
+Route::group(['middleware' => 'jwt.auth','prefix' => '/v1', 'namespace' => 'Api\V1', 'as' => 'api.'], function () {
     Route::resource('companies', 'CompaniesController', ['except' => ['create', 'edit']]);
 });
